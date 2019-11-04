90c5b65b155cde1e8db6201fbaae635699213627
Kaustubha DS <dskaustubh@gmail.com>
2019-11-04 18:50:47 +0530

    index route

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import hashlib
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 if __name__=="__main__":
     app.run(debug=True)
