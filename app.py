07a7db0c809d49892c163a2c049c1821195ed3f4
Manish <mangreat64@gmail.com>
2019-11-04 22:16:05 +0530

    Added front end codes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,request,session,redirect
+from flask import Flask,render_template,request,session,url_for,redirect
 import hashlib
-app =Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app = Flask(__name__)
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', methods = ['GET'])
 
-if __name__=="__main__":
-    app.run(debug=True)
+# if __name__=="__main__":
+app.run(debug=True)
