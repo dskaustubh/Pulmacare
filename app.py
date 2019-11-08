678be4fb7cf119916a2cd177dbaea5b44a4ec76b
Manish <mangreat64@gmail.com>
2019-11-08 17:30:02 +0530

    Login and signup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,session,redirect,jsonify,url_for,flash
 
-# import pymysql.cursors
+import pymysql.cursors
 import hashlib
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -11,13 +11,13 @@ email=""
 psw=""
 conf_psw=""
 
-# connection = pymysql.connect(host='localhost',
-#                              user='root',
-#                              password='',
-#                              db='pulmacare',
-#                              charset='utf8mb4',
-#                              cursorclass=pymysql.cursors.DictCursor,
-#                              autocommit=True)
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='pulmacare',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor,
+                             autocommit=True)
 
 @app.route("/")
 def index():
@@ -39,36 +39,36 @@ def login():
         return redirect(url_for('index'))    
 
     
-# @app.route("/register",methods = ['POST','GET'])
-# def signup():
-#     if request.method == 'GET':
-#         return render_template("register.html")
-#     #role 1 for hospital,2 for  patient,3 for docs
-#     data=request.get_json()
-#     print(data)
-#     with connection.cursor() as cursor:
-#         robj= dict()
-#         robj['success']=False
-#         try:
-#             phash=hashlib.md5(data['password'].encode())
-#             phash=phash.hexdigest()
-#             print(phash)
-#             role=data['role']
-#             cursor.execute("insert into users (name,email,password,role) values(%s,%s,%s,%s)",(data['name'], data['email'],phash,data['role']))
-#             if role==1:
-#                 #hospital
-#                 pass
-#             elif role==2:
-#                 #patient
-#                 pass
-#             else:
-#                 #doctor
-#                 pass
+@app.route("/register",methods = ['POST','GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template("register.html")
+    #role 1 for hospital,2 for  patient,3 for docs
+    data=request.get_json()
+    print(data)
+    with connection.cursor() as cursor:
+        robj= dict()
+        robj['success']=False
+        try:
+            phash=hashlib.md5(data['password'].encode())
+            phash=phash.hexdigest()
+            print(phash)
+            role=data['role']
+            cursor.execute("insert into users (name,email,password,role) values(%s,%s,%s,%s)",(data['name'], data['email'],phash,data['role']))
+            if role==1:
+                #hospital
+                pass
+            elif role==2:
+                #patient
+                pass
+            else:
+                #doctor
+                pass
 
-#             robj['success']=True
+            robj['success']=True
             
-#         finally:
-#             return jsonify(robj)
+        finally:
+            return jsonify(robj)
 
     
 
