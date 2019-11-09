69a67f7e564df4f167619ccd8be504150a468fcc
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 21:47:08 +0530

    it works

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import predict
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing import image
 from werkzeug.utils import secure_filename
@@ -156,15 +157,18 @@ def logout():
 @app.route("/uploadxray",methods=['POST'])
 def uploadxray():
     #pred=predict.prediction()
-    model = load_model('model.h5')
-    img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = x/255.0
-    pred=model.predict_proba(x)
-    resp=dict()
-    resp['pred']=pred[0]
-    return jsonify(resp)
+    model = tf.keras.models.load_model('model.h5')
+    return("hi")
+    # img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
+    # x = image.img_to_array(img)
+    # x = np.expand_dims(x, axis=0)
+    # x = x/255.0
+    # pred=model.predict_proba(x)
+    # print(pred)
+    #resp=dict()
+    #resp['pred']=pred[0]
+    return("hi")
+    # return resp
 model = load_model('model.h5')
 img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
 x = image.img_to_array(img)
