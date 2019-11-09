054296307bd348b998b0c442f3ce88a5ba773f1d
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 21:22:51 +0530

    8 yr old bays

--- app.py
+++ app.py
@@ -165,6 +165,13 @@ def uploadxray():
     resp=dict()
     resp['pred']=pred[0]
     return jsonify(resp)
+model = load_model('model.h5')
+img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = x/255.0
+pred=model.predict_proba(x)
+print(pred)
     
     
 
