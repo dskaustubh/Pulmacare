b2fdbe03ac31d8a5b0dddf4a71b842921bb891c0
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 07:49:33 +0530

    predict.py working

--- predict.py
+++ predict.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 from keras.preprocessing import image
 model = load_model('model.h5')
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
 img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
