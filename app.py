07eb967b1424523c981e4843894a9fb2ac0ce2f1
Manish <mangreat64@gmail.com>
2019-11-10 00:15:52 +0530

    push

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask,render_template,request,session,redirect,jsonify,url_for
 import pymysql.cursors
 import hashlib
 import os
-import numpy as np
-import pandas as pd
-from keras.models import load_model
-from keras.preprocessing import image
+import predict
+# import numpy as np
+# import pandas as pd
+# from keras.models import load_model
+# from keras.preprocessing import image
 from werkzeug.utils import secure_filename
 app =Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -65,9 +66,6 @@ def signup():
             
         finally:
             return redirect(url_for("login_page"))
-@app.route("/uploadxray",methods=['POST'])
-def uploadxray():
-    pass
 
 @app.route("/login",methods = ['POST'])
 def login():
@@ -155,6 +153,21 @@ def logout():
     session.clear()
     return render_template("index.html")
 
+@app.route("/uploadxray",methods=['POST'])
+def uploadxray():
+    #pred=predict.prediction()
+    # model = load_model('model.h5')
+    # img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
+    # x = image.img_to_array(img)
+    # x = np.expand_dims(x, axis=0)
+    # x = x/255.0
+    # pred=model.predict_proba(x)
+    # resp=dict()
+    # resp['pred']=pred[0]
+    # return jsonify(resp)
+    
+    
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,threaded=False)
