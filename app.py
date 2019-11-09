5db4b48bb8423e67420f8c609f695a9c5e1b8444
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 21:48:51 +0530

    hereee weee gooo

--- app.py
+++ app.py
@@ -168,14 +168,6 @@ def uploadxray():
     #resp=dict()
     #resp['pred']=pred[0]
     return("hi")
-    # return resp
-model = load_model('model.h5')
-img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = x/255.0
-pred=model.predict_proba(x)
-print(pred)
     
     
 
