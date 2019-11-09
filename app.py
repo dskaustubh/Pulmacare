ef793962f562bbce1daba34b8ca190e3137187e8
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 21:59:55 +0530

    idk what this is

--- app.py
+++ app.py
@@ -158,19 +158,13 @@ def logout():
 def uploadxray():
     #pred=predict.prediction()
     model = tf.keras.models.load_model('model.h5')
-    return("hi")
     img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = x/255.0
-    # pred=model.predict_proba(x)
-    # print(pred)
-    #resp=dict()
-    #resp['pred']=pred[0]
+    pred=model.predict_proba(x)
+    print(pred[0])
     return("hi")
     
-    
-
-
 if __name__=="__main__":
     app.run(debug=True,threaded=False)
