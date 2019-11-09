478335accc0f06aed15acfeed3f831c165f989ed
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 21:50:12 +0530

    giroud

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def uploadxray():
     model = tf.keras.models.load_model('model.h5')
     return("hi")
     img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
-    # x = image.img_to_array(img)
+    x = image.img_to_array(img)
     # x = np.expand_dims(x, axis=0)
     # x = x/255.0
     # pred=model.predict_proba(x)
