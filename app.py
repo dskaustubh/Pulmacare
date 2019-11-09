c5f9054eb5d54a563d6fc7f76620de7f0f8d3027
Kaustubha DS <dskaustubh@gmail.com>
2019-11-09 22:15:02 +0530

    if it isnt brokennn why fix it

--- app.py
+++ app.py
@@ -162,12 +162,19 @@ def uploadxray():
     file.save(filename)
     #pred=predict.prediction()
     model = tf.keras.models.load_model('model.h5')
-    img = image.load_img('test_3.jpeg', target_size=(100,100))#Pneumonia Image 
+    img = image.load_img(filename,target_size=(100,100))#Pneumonia Image 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = x/255.0
     pred=model.predict_proba(x)
     print(pred[0])
+    eden=str(pred[0])
+    eden=eden.split(' ')[0]
+    hazard=eden[1:]
+    hazard=float(hazard)
+    print(hazard)
+
+
     return("hi")
     
 if __name__=="__main__":
